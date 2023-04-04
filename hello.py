ba4b785121da8fe7387d72b73481efd342e33c9b
MandalorianY <amaurylange@gmail.com>
2023-04-04 12:01:23 +0200

    test

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask,request
+from flask import Flask, request
 import os
 import openai
 
@@ -11,10 +11,11 @@ openai.api_key = os.environ.get('OPENAI_KEY')
 def index():
     return "<h1>Hello, World!</h1>"
 
+
 @app.route('/chatgpt')
 def chatgpt():
     args = request.args
-    message =args.get("message")
+    message = args.get("message")
     print(message)
     completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
@@ -23,3 +24,17 @@ def chatgpt():
     return completion['choices'][0]['message']['content']
 
 
+@app.route('/generate_code')
+def generate_code():
+    args = request.args
+    coding_language = args.get("coding_language")
+    code_request = args.get("code_request")
+
+    message = f"Write {code_request} in {coding_language}."
+    print(message)
+
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": message}]
+    )
+    return completion['choices'][0]['message']['content']
